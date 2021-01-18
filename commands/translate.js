f0aae84d43eebcfb737af1d0208a0ac543767d6f
jbmagination <26027089+jbmagination@users.noreply.github.com>
2021-01-18 14:41:05 -0500

    Upload test

---
+++ commands/translate.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: 'translate',
+	description: 'Translate between the Galactic Alphabet and English!',
+	execute(message, args) {
+		message.channel.send('Just a simple test!');
+	},
+};
